85cf163f83d5644a5f613882dc64faae0c59dfbf
Jesse Coretta <{ID}+{username}@users.noreply.github.com>
2025-07-05 03:58:55 -0700

    add debug calls

--- ber.go
+++ ber.go
@@ -11,7 +11,7 @@ import (
 
 /*
 BERPacket implements a data encapsulation and transport type to store
-[ITU-T Rec. X.690] [BER]-encoded content. It qualifies the [Packet]
+[ITU-T Rec. X.690] [BER]-encoded content. It qualifies the [PDU]
 interface.
 
 Instances of this type may be created using [Marshal] with the [BER]
@@ -23,15 +23,24 @@ type.
 [ITU-T Rec. X.690]: https://www.itu.int/rec/T-REC-X.690
 */
 type BERPacket struct {
+	id     string
 	data   []byte
 	offset int
 }
 
 /*
 Type returns [BER], identifying the receiver as an ASN.1 Basic Encoding
-Rules [Packet] qualifier.
+Rules [PDU] qualifier.
 */
 func (r BERPacket) Type() EncodingRule { return BER }
+
+/*
+ID returns the unique string identifier associated with the receiver instance.
+
+Note that if this package is not compiled or run with "-tags asn1debug", this
+method will always return a zero string.
+*/
+func (r BERPacket) ID() string { return r.id }
 
 /*
 Class returns an integer alongside an error following an attempt to parse
@@ -104,15 +113,23 @@ func (r BERPacket) Data() []byte { return r.data }
 Append appends data to the receiver instance.
 */
 func (r *BERPacket) Append(data ...byte) {
+	id := newLItem(r.ID(), r.Type(), "PDU")
+	debugEnter(data)
+	defer func() { debugExit() }()
+
 	if r == nil || len(data) == 0 {
 		return
 	}
 	need := r.Len() + len(data)
 
-	if cap(r.data) < need {
+	if c := cap(r.data); c < need {
+		debugTrace(id, newLItem(need, "need"), newLItem(c, "cap"))
+
 		bufPtr := bufPool.Get().(*[]byte)
 		if cap(*bufPtr) < need {
-			*bufPtr = make([]byte, 0, need*2)
+			n2 := need * 2
+			debugTrace(id, newLItem(n2, "alloc new cap"))
+			*bufPtr = make([]byte, 0, n2)
 		}
 		newBuf := append((*bufPtr)[:0], r.data...)
 
@@ -121,9 +138,13 @@ func (r *BERPacket) Append(data ...byte) {
 			bufPool.Put(&old)
 		}
 		r.data = newBuf
+		debugEvent(EventTrace|EventPDU,
+			id, newLItem(r.data, "after growth"))
 	}
 
 	r.data = append(r.data, data...)
+	debugEvent(EventTrace|EventPDU,
+		id, newLItem(r.data, "after append"))
 }
 
 /*
@@ -142,30 +163,49 @@ for some reason, or if it is necessary to write additional data.
 Supplying an integer of negative one (-1) will set the offset to the final character in the
 underlying buffer if non-zero in length.
 
-If no variadic input is provided,, the offset position index is set to zero (0).
+If no variadic input is provided, the offset position index is set to zero (0).
 */
-func (r *BERPacket) SetOffset(offset ...int) { setPacketOffset(r, offset...) }
+func (r *BERPacket) SetOffset(offset ...int) { r.offset = setPacketOffset(r, offset...) }
 
 /*
 Free frees the receiver instance.
 */
 func (r *BERPacket) Free() {
-	if cap(r.data) != 0 {
+	debugEnter(r)
+	defer func() { debugExit() }()
+
+	if c := cap(r.data); c != 0 {
+		debugEvent(EventTrace|EventPDU,
+			r, newLItem(c, "release cap"))
 		buf := r.data[:0]
 		bufPool.Put(&buf)
 	}
+
+	lb := r.Len()
 	*r = BERPacket{}
-	berPktPool.Put(r)
+	debugEvent(EventTrace|EventPDU,
+		r, newLItem([]int{lb, r.Len()}, "free b/a len"))
+
+	bcdPktPool.Put(r)
 }
 
 /*
 PeekTLV returns [TLV] alongside an error. This method is similar to the standard
-[Packet.TLV] method, except this method does not advance the offset.
+[PDU.TLV] method, except this method does not advance the offset.
 */
 func (r *BERPacket) PeekTLV() (TLV, error) {
+	var tlv TLV
+	var err error
+	debugEnter(r)
+	defer func() {
+		debugExit(tlv, newLItem(err))
+	}()
+
 	sub := r.Type().New(r.Data()...)
+	sub.(*BERPacket).id = r.id
 	sub.SetOffset(r.Offset())
-	return getTLV(sub, nil)
+	tlv, err = getTLV(sub, nil)
+	return tlv, err
 }
 
 /*
@@ -180,38 +220,54 @@ header to the receiver buffer.
 */
 func (r *BERPacket) WriteTLV(tlv TLV) error { return writeTLV(r, tlv, nil) }
 
-func newBERPacket(src ...byte) (pkt Packet) {
-	b := berPktPool.Get().(*BERPacket)
+func newBERPacket(src ...byte) (pkt PDU) {
+	debugEnter(src)
+	defer func() {
+		debugExit(pkt)
+	}()
 
-	if cap(b.data) < len(src) {
+	b := bcdPktPool.Get().(*BERPacket)
+	b.id = makePacketID()
+	debugTrace(b.id, "New"+b.Type().String()+"PDU")
+
+	cl, sl := cap(b.data), len(src)
+	debugTrace(newLItem(b, "from pool"), newLItem([]int{cl, sl}, "cap/len"))
+
+	if cl < sl {
+		debugTrace(b, newLItem([]int{sl, cl}, "have/need cap"))
 		bufPtr := bufPool.Get().(*[]byte)
-		if cap(*bufPtr) < len(src) {
-			*bufPtr = make([]byte, 0, roundup(len(src)))
+		if cap(*bufPtr) < sl {
+			newCap := roundup(sl)
+			debugTrace(b, newLItem(newCap, "alloc buf (new cap)"))
+			*bufPtr = make([]byte, 0, newCap)
 		}
 		b.data = *bufPtr
+		cl = cap(b.data)
+		debugTrace(b, newLItem(cl, "new buf cap"))
 	}
-
 	b.data = append(b.data[:0], src...)
+	debugTrace(b, newLItem(b.data, "load"), newLItem(len(b.data), "len"))
 	pkt = b
 	return
 }
 
 /*
-Packet returns an instance of [Packet] alongside an error following an attempt to read
-the next L (length) bytes from the receiver instance into the return [Packet].
+Deprecated: Packet returns an instance of [PDU] alongside an error following an
+attempt to read the next L (length) bytes from the receiver instance into the
+return [PDU].
 
-This method is used in cases where a constructed type, i.e.: a SEQUENCE (struct), resides
-within another constructed type via nesting.
+This method is used in cases where a constructed type, i.e.: a SEQUENCE (struct),
+resides within another constructed type via nesting.
 
-Note that successful use of this method shall advance the offset to the end of the extracted
-[Packet] (position offset+length). The receiver offset is not modified.
+Note that successful use of this method shall advance the offset to the end of the
+extracted [PDU] (position offset+length). The receiver offset is not modified.
 */
-func (r *BERPacket) Packet(L int) (Packet, error) { return extractPacket(r, L) }
+func (r *BERPacket) Packet(L int) (PDU, error) { return extractPacket(r, L) }
 
 /*
 // TODO: reinvestigate this
 //
-func readIndefiniteContents(r Packet, d []byte) ([]byte, int, error) {
+func readIndefiniteContents(r PDU, d []byte) ([]byte, int, error) {
 	off := r.Offset()
 	var val []byte
 	var err error
@@ -233,9 +289,45 @@ func readIndefiniteContents(r Packet, d []byte) ([]byte, int, error) {
 func encodeBERLengthInto(dst *[]byte, n int) {
 	if n == -1 { // indefinite
 		*dst = append(*dst, 0x80)
+		debugCodec(newLItem(*dst, BER, "INDEFINITE-LENGTH"))
 		return
 	}
-	encodeDERLengthInto(dst, n)
+	encodeBCDLengthInto(dst, n)
 }
 
-var berPktPool = sync.Pool{New: func() any { return &BERPacket{} }}
+func encodeBCDLengthInto(dst *[]byte, n int) {
+	debugEnter(newLItem(n, "n"))
+	defer func() { debugExit() }()
+
+	defer debugPath(*dst, newLItem(n, "len"))()
+
+	if n < 128 { // short form
+		*dst = append(*dst, byte(n))
+		debugCodec(newLItem(n, "short form"))
+		return
+	}
+	debugCodec(newLItem(n, "long form"))
+
+	// long form – emit the *minimal* number of octets
+	var tmp [8]byte // handles 64-bit length
+	i := len(tmp)
+	for n > 0 {
+		i--
+		tmp[i] = byte(n)
+		n >>= 8
+	}
+	byteCount := len(tmp) - i
+	debugCodec(newLItem(byteCount, "octets"))
+
+	*dst = append(*dst, 0x80|byte(len(tmp)-i))
+	*dst = append(*dst, tmp[i:]...)
+
+	debugCodec(newLItem(tmp[i:byteCount+i], "encoded length"))
+}
+
+var bcdPktPool = sync.Pool{New: func() any { return &BERPacket{} }}
+
+func init() {
+	activeEncodingRules |= BER
+	pDUConstructors[BER] = newBERPacket
+}
